79a681aaa728f2da717398ec48b7f5dc72b0afac
Daniel Schewe <danielschewe@Daniels-MacBook-Pro.local>
2019-05-17 17:57:20 -0600

    Friday update

--- app/app.js
+++ app/app.js
@@ -45,6 +45,22 @@ var keyExists = function(key) {
 ///////////////////////////////////////////
 //event handlers for the buttons and ... possibly the inputboxes
   //preventdefault on button clicks
+
+
+// user enters in item quantity and name, then chooses department
+// print information in list form below input space
+// store item name and department as key:value in local storage
+// if user hits clear list -> clear local storage data
+// create checkbox in front of item so user can check off when it's been 'handled' at the store
+// this is all AFTER cutting scope in half x 2
+// won't include the following unless time allows
+// print grid to screen and sort by department (and alphabetically)
+// show purchased items with strike-through
+// be able to put in recurring items and quantities
+// share list with someone else (live)
+// ...
+
+
 $(document).ready(function() {
   $('#createButton').click(function(event) {
     event.preventDefault();
@@ -71,7 +87,9 @@ $(document).ready(function() {
   });
 
   $('#addItemButton').click(function(event) {
-    return window.localStorage.setItem(key, value);
+  var printItem = $('<div class="item"</div>');
+  printItem.appendTo("#item-container");
+  createItem(currentKey, currentValue);
   });
 
   $('#clearListButton').click(function(event) {
@@ -85,5 +103,32 @@ $(document).ready(function() {
 });
 
 
+
+// FROM DAVE'S ORIGINAL APP.JS
+// $(document).ready(function() {
+//   $('#createButton').click(function(event) {
+//     event.preventDefault();
+
+//     var currentKey = $("#keyInput").val();
+//     var currentValue = $("#valueInput").val();
+//     if (keyExists(currentKey)) {
+//       //current key exists, do something error-handle-y
+//     } else {
+//       createItem(currentKey, currentValue);
+//     }
+//   });
+
+//   $('#updateButton').click(function(event) {
+//     event.preventDefault();
+
+//     var currentKey = $("#keyInput").val();
+//     var currentValue = $("#valueInput").val();
+//     if (keyExists(currentKey)) {
+//       updateItem(currentKey, currentValue);
+//     } else {
+//       //current key doesnt exist, do stuff
+//     }
+//   });
+// });
 
 
